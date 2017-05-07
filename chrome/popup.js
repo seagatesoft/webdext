3afec5d67a21196a9ad341a9803dfe4878333ada
Sigit Dewanto <sigitdewanto11@yahoo.co.uk>
2017-05-07 08:15:33 +0700

    Intelligent Extract button -> add "please wait" and disable button after clicked, restore the label and enable again after intelligent extraction finished.

--- chrome/popup.js
+++ chrome/popup.js
@@ -1,5 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("intelligentExtractButton").addEventListener("click", function() {
-        chrome.tabs.executeScript(null, {file: "webdext.js"});
+    document.getElementById("intelligentExtractButton").addEventListener("click", function(event) {
+        var button = document.getElementById("intelligentExtractButton");
+        button.disabled = true;
+        button.innerText = "Please wait";
+        chrome.tabs.executeScript(null, {file: "webdext.js"}, function () {
+            var button = document.getElementById("intelligentExtractButton");
+            button.disabled = false;
+            button.innerText = "Intelligent Extract";
+        });
     });
 });
