e4d3fcfe02e70d2df374167701510dfc5b249d4c
Sigit Dewanto <sigitdewanto11@yahoo.co.uk>
2017-06-11 18:58:48 +0700

    Implement export to CSV.

--- chrome/exportdata.js
+++ chrome/exportdata.js
@@ -1,8 +1,29 @@
+function convertToCSV(dataRecords) {
+    var dataRecordsLength = dataRecords.length,
+        csvArray = [];
+
+    for (var i=0; i < dataRecordsLength; i++) {
+        var keys = Object.keys(dataRecords[i]),
+            keysLength = keys.length,
+            values = [];
+
+        for (var j=0; j < keysLength; j++)  {
+            var value = '"' + dataRecords[i][keys[j]].value + '"';
+            values.push(value);
+        }
+
+        csvArray.push(values.join(","));
+    }
+
+    return csvArray.join("\n");
+}
+
 chrome.runtime.sendMessage({info: "exportDataPageLoaded"}, function(response) {
     var exportedDataTextarea = document.getElementById("exportedDataTextarea");
 
     if (response.data.dataType === "json") {
         exportedDataTextarea.innerText = JSON.stringify(response.data.dataRecords);
     } else if (response.data.dataType === "csv") {
+        exportedDataTextarea.innerText = convertToCSV(response.data.dataRecords);
     }
 });
